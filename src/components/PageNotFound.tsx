393344dae0f5d4e73bf730a8cea8636300cedb0d
Nisha Chavan <nisha.chavan@lekab.com>
2023-12-06 09:59:04 +0100

    framer motion installed and intial basic use of it in PageNot found

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 function PageNotFound() {
     return (
-        <>
+        <motion.div
+                    whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+                    transition={{ duration: 0.5 }}
+        >
             <h1>404 - Page not Found</h1>
             <p>Please check the path</p>
-        </>
+        </motion.div>
     );
 }
 export default PageNotFound;
