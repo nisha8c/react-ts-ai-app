9a0f4d6ee6e8e3fb3c3f096e1d638e851377b18f
Nisha Chavan <nisha.chavan@lekab.com>
2023-12-13 11:49:31 +0100

    added rest api for countries

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,11 +1,22 @@
 import {UserButton} from "@clerk/clerk-react";
-import React from "react";
+import React, {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import Button from "@mui/material/Button";
 import AppWrap from "../wrapper/AppWrap";
 
 function LandingPage() {
     const navigate = useNavigate();
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => console.log('DATA::: ', data))
+            .catch((error) => console.error('There has been a problem with your fetch operation: ', error));
+    }, []);
     return (
         <div className={"landing-page"}>
             <h1>Protected page</h1>
