1e31243864010ae9e486604cf7e0689d15eeac3d
Nisha Chavan <nisha.chavan@lekab.com>
2023-12-13 08:49:31 +0100

    keeping only userButton

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,18 +1,18 @@
-import {SignOutButton, UserButton} from "@clerk/clerk-react";
+import {UserButton} from "@clerk/clerk-react";
 import React from "react";
 import {useNavigate} from "react-router-dom";
 import Button from "@mui/material/Button";
+import AppWrap from "../wrapper/AppWrap";
 
 function LandingPage() {
     const navigate = useNavigate();
     return (
-        <>
+        <div className={"landing-page"}>
             <h1>Protected page</h1>
             <UserButton />
-            <SignOutButton />
             <Button onClick={() => navigate('/page1')}>Go to Page 1</Button>
             <Button onClick={() => navigate('/page2')}>Go to Page 2</Button>
-        </>
+        </div>
     );
 }
-export default LandingPage;
+export default AppWrap(LandingPage, 'landing-page', 'landing-page');
