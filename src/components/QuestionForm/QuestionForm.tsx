d67fb6795756f3bce20604f26342ea87c12d1a21
Nisha Chavan <nisha.chavan@lekab.com>
2023-12-21 16:24:38 +0100

    VOICE recognition added

--- src/components/QuestionForm/QuestionForm.tsx
+++ src/components/QuestionForm/QuestionForm.tsx
@@ -1,6 +1,6 @@
-import React, {ChangeEvent, FormEvent, useEffect, useRef} from 'react';
+import React, {ChangeEvent, FormEvent, useEffect} from 'react';
 import { InputLabel, TextField, Button } from '@mui/material';
-
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 interface QuestionFormProps {
     name: string;
     handleSubmit: (event: FormEvent) => void;
@@ -9,37 +9,73 @@ interface QuestionFormProps {
     isDisabled: boolean;
     score: number;
 }
-const QuestionForm: React.FC<QuestionFormProps> = ({ name, handleSubmit, userAnswer, handleInputChange, isDisabled, score }) => {
+const QuestionForm: React.FC<QuestionFormProps> = ({name, handleSubmit, userAnswer, handleInputChange, isDisabled, score }) => {
     const inputRef = React.useRef<HTMLInputElement | null>(null);
+
+    const {
+        transcript,
+        resetTranscript,
+        listening,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
 
     useEffect(() => {
         if (inputRef.current) {
             inputRef.current!.focus();
         }
     }, [name]); // refocus whenever the name changes
+
+    const handleVoiceButtonClick = () => {
+        resetTranscript();
+        SpeechRecognition.startListening(); // Use the startListening function from SpeechRecognition
+    };
+
+    const handleVoiceInputStop = () => {
+        SpeechRecognition.stopListening(); // Use the stopListening function from SpeechRecognition
+        handleInputChange({
+            target: { value: transcript },
+        } as React.ChangeEvent<HTMLInputElement>);
+    };
+
+    useEffect(() => {
+        handleInputChange({
+            target: { value: transcript },
+        } as React.ChangeEvent<HTMLInputElement>);
+    }, [transcript]);
+
+
     return (
         <form onSubmit={handleSubmit} className={'form'}>
-            <InputLabel>
-                What is the capital of {name}?
-            </InputLabel>
+            <InputLabel>What is the capital of {name}?</InputLabel>
 
-            <TextField id="outlined-basic"
-                       label="Enter Your Answer Here..."
-                       autoFocus={true}
-                       variant="outlined"
-                       value={userAnswer}
-                       type={"text"}
-                       onChange={handleInputChange}
-                       disabled={isDisabled}
-                       inputRef={inputRef}
+            <TextField
+                id="outlined-basic"
+                label="Enter Your Answer Here..."
+                autoFocus={true}
+                variant="outlined"
+                value={userAnswer}
+                type="text"
+                onChange={handleInputChange}
+                disabled={isDisabled}
+                inputRef={inputRef}
             />
 
-            <Button
-                type="submit"
-                variant="outlined"
-                size={'large'}
-                disabled={isDisabled}
-            >
+            {browserSupportsSpeechRecognition && (
+                <>
+                    <Button type="button" variant="outlined" size={'large'} onClick={handleVoiceButtonClick}>
+                        Start Voice Input
+                    </Button>
+
+                    {listening && (
+                        <Button type="button" variant="outlined" size={'large'} onClick={handleVoiceInputStop}>
+                            Stop Voice Input
+                        </Button>
+                    )}
+                </>
+            )}
+
+            <Button type="submit" variant="outlined" size={'large'} disabled={isDisabled}>
                 Submit
             </Button>
 
@@ -47,5 +83,6 @@ const QuestionForm: React.FC<QuestionFormProps> = ({ name, handleSubmit, userAns
         </form>
     );
 };
+
 
 export default QuestionForm;
