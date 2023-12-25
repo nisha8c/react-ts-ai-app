b07355e4682fff57fcab4f4c60dc052e59bf6251
Nisha Chavan <nisha.chavan@lekab.com>
2023-12-25 20:06:13 +0100

    responsive chart option added

--- src/containers/WeeklyChart/WeeklyChart.tsx
+++ src/containers/WeeklyChart/WeeklyChart.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useCallback } from 'react';
+import React, {useEffect, useRef, useCallback, useState} from 'react';
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import { useSelector } from 'react-redux';
 import { Bar } from 'react-chartjs-2';
-import { parse, isValid } from 'date-fns';
 
 import AppWrap from "../../wrapper/AppWrap";
 import { Chart, registerables } from 'chart.js';
+import { getISOWeek } from 'date-fns';
 import './WeeklyChart.scss';
 import {GameEntry} from "../../types/types";
 
@@ -16,13 +16,10 @@ let myChart: Chart | null = null;
 
 const WeeklyChart = () => {
     const gameHistory = useSelector((state: any) => state.historyReducer.gameHistory);
-    console.log('gameHistory:::: ', gameHistory);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-
     // Create a new array that only includes entries with a non-null, non-empty level
     const validEntries = gameHistory.filter((entry: GameEntry) => entry.level);
-    console.log('All dates:', validEntries.map((entry: GameEntry) => entry.timestamp));
-
+    //console.log('All dates:', validEntries.map((entry: GameEntry) => entry.timestamp));
 
     const destroyChart = useCallback(() => {
         if (myChart) {
@@ -31,34 +28,6 @@ const WeeklyChart = () => {
         }
     }, []);
 
-    /*
-    const calculateTotalScore = (level: string, day: number) => {
-        console.log('validEntries:: ', validEntries)
-        const filteredEntries = validEntries
-            .filter((entry: GameEntry) => {
-
-                // Check if entry.timestamp is not undefined and not an empty string
-                if (!entry.timestamp || entry.timestamp.trim() === '') {
-                    console.log(`Invalid timestamp: ${entry.timestamp}`);
-                    return false;
-                }
-
-                // Convert the timestamp to a standard format
-                const entryDate = new Date(entry.timestamp);
-                console.log('entryDate:::::::::::::::::: ', entryDate)
-
-                // Check if entry.level matches the given level and entry.timestamp matches the given day
-                return entry.level === level && entryDate.getUTCDay() === Number(day);
-            });
-
-        const totalScore = filteredEntries
-            .reduce((total: number, entry: GameEntry) => total + entry.score, 0);
-        console.log(`Total score for level ${level} on day ${day}:`, totalScore);
-        return totalScore;
-    };
-
-     */
-    /*
     const calculateTotalScore = (level: string, day: number) => {
         console.log('validEntries:: ', validEntries);
         const filteredEntries = validEntries
@@ -70,134 +39,6 @@ const WeeklyChart = () => {
                     return false;
                 }
 
-                // Parse the timestamp using date-fns parse function
-                const entryDate = parse(entry.timestamp, 'dd/MM/yyyy, HH:mm:ss', new Date());
-
-                // Check if the parsed date is valid
-                if (!isValid(entryDate)) {
-                    console.log('Invalid date:', entryDate);
-                    return false;
-                }
-
-                // Check if entry.level matches the given level and entry.timestamp matches the given day
-                const isMatch = entry.level === level && entryDate.getDate() === Number(day);
-                console.log('Parsed date:', entryDate, 'Comparison:', isMatch ? 'Match' : 'No Match');
-
-                return isMatch;
-            });
-
-        const totalScore = filteredEntries
-            .reduce((total: number, entry: GameEntry) => total + entry.score, 0);
-        console.log(`Total score for level ${level} on day ${day}:`, totalScore);
-        return totalScore;
-    };
-
-     */
-
-    /* -worked
-    const calculateTotalScore = (level: string, day: number) => {
-        console.log('validEntries:: ', validEntries);
-        const filteredEntries = validEntries
-            .filter((entry: GameEntry) => {
-
-                // Check if entry.timestamp is not undefined and not an empty string
-                if (!entry.timestamp || entry.timestamp.trim() === '') {
-                    console.log(`Invalid timestamp: ${entry.timestamp}`);
-                    return false;
-                }
-
-                // Log the original timestamp
-                console.log('Original timestamp:', entry.timestamp);
-
-                // Split the timestamp into date and time components
-                const [dateStr, timeStr] = entry.timestamp.split(', ');
-                const [dayStr, monthStr, yearStr] = dateStr.split('/');
-
-                // Create a new Date object using the adjusted order of month and day
-                const entryDate = new Date(`${monthStr} ${dayStr} ${yearStr} ${timeStr}`);
-
-                // Check if entry.level matches the given level and entry.timestamp matches the given day
-                const entryDay = entryDate.getDay(); // 0-6, where 0 is Sunday
-                const isMatch = entry.level === level && entryDay === Number(day);
-                console.log('Parsed date:', entryDate, 'Comparison:', isMatch ? 'Match' : 'No Match');
-
-                return isMatch;
-            });
-
-        const totalScore = filteredEntries
-            .reduce((total: number, entry: GameEntry) => total + entry.score, 0);
-        console.log(`Total score for level ${level} on day ${day}:`, totalScore);
-        return totalScore;
-    };
-
-     */
-
-    /* - worked too
-    const calculateTotalScore = (level: string, day: number) => {
-        console.log('validEntries:: ', validEntries);
-        const filteredEntries = validEntries
-            .filter((entry: GameEntry) => {
-
-                // Check if entry.timestamp is not undefined and not an empty string
-                if (!entry.timestamp || entry.timestamp.trim() === '') {
-                    console.log(`Invalid timestamp: ${entry.timestamp}`);
-                    return false;
-                }
-
-                // Log the original timestamp
-                console.log('Original timestamp:', entry.timestamp);
-
-                // Attempt to parse the timestamp as DD/MM/YYYY, HH:mm:ss
-                const [dateStr, timeStr] = entry.timestamp.split(', ');
-                const [dayStr, monthStr, yearStr] = dateStr.split('/');
-                const parsedDate = new Date(`${monthStr} ${dayStr} ${yearStr} ${timeStr}`);
-
-                // Check if the parsed date is valid
-                if (!isNaN(parsedDate.getTime())) {
-                    // Successfully parsed as DD/MM/YYYY, HH:mm:ss
-                    console.log('Parsed date:', parsedDate);
-                    const entryDay = parsedDate.getDay(); // 0-6, where 0 is Sunday
-                    const isMatch = entry.level === level && entryDay === Number(day);
-                    console.log('Comparison:', isMatch ? 'Match' : 'No Match');
-                    return isMatch;
-                } else {
-                    // Try parsing the timestamp as ISO 8601
-                    const isoDate = new Date(entry.timestamp);
-                    if (!isNaN(isoDate.getTime())) {
-                        // Successfully parsed as ISO 8601
-                        console.log('Parsed date (ISO 8601):', isoDate);
-                        const entryDay = isoDate.getDay(); // 0-6, where 0 is Sunday
-                        const isMatch = entry.level === level && entryDay === Number(day);
-                        console.log('Comparison (ISO 8601):', isMatch ? 'Match' : 'No Match');
-                        return isMatch;
-                    } else {
-                        // Both parsing attempts failed
-                        console.log('Invalid timestamp format');
-                        return false;
-                    }
-                }
-            });
-
-        const totalScore = filteredEntries
-            .reduce((total: number, entry: GameEntry) => total + entry.score, 0);
-        console.log(`Total score for level ${level} on day ${day}:`, totalScore);
-        return totalScore;
-    };
-
-     */
-
-    const calculateTotalScore = (level: string, day: number) => {
-        console.log('validEntries:: ', validEntries);
-        const filteredEntries = validEntries
-            .filter((entry: GameEntry) => {
-
-                // Check if entry.timestamp is not undefined and not an empty string
-                if (!entry.timestamp || entry.timestamp.trim() === '') {
-                    console.log(`Invalid timestamp: ${entry.timestamp}`);
-                    return false;
-                }
-
-                // Log the original timestamp
                 console.log('Original timestamp:', entry.timestamp);
 
                 // Attempt to parse the timestamp as DD/MM/YYYY, HH:mm:ss
@@ -257,6 +98,10 @@ const WeeklyChart = () => {
             if (ctx) {
                 myChart = new Chart(ctx, {
                     type: 'bar',
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: true,
+                    },
                     data: {
                         labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
                         datasets: [
@@ -286,8 +131,6 @@ const WeeklyChart = () => {
                 });
             }
         }
-
-        console.log('gameHistory:', gameHistory);
         // Cleanup function to destroy the chart when the component is unmounted or dependencies change
         return destroyChart;
     }, [canvasRef, gameHistory, destroyChart]);
