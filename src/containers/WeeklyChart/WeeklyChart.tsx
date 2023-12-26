758440533b4885c0275c3c9dd85b33bf4bd0781e
Nisha Chavan <nisha.chavan@lekab.com>
2023-12-26 17:20:15 +0100

    next and prev buttons added with week update. - need to be tested

--- src/containers/WeeklyChart/WeeklyChart.tsx
+++ src/containers/WeeklyChart/WeeklyChart.tsx
@@ -1,12 +1,14 @@
+// import {ArrowLeft, ArrowRight} from "@mui/icons-material";
+
 import React, {useEffect, useRef, useCallback, useState} from 'react';
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import {Button, IconButton} from "@mui/material";
 import { useSelector } from 'react-redux';
 import { Bar } from 'react-chartjs-2';
-
+import { getISOWeek, startOfWeek, endOfWeek, isWithinInterval, addWeeks, subWeeks } from 'date-fns';
+import {ArrowLeft, ArrowRight} from "@mui/icons-material";
 import AppWrap from "../../wrapper/AppWrap";
 import { Chart, registerables } from 'chart.js';
-import { getISOWeek } from 'date-fns';
 import './WeeklyChart.scss';
 import {GameEntry} from "../../types/types";
 
@@ -17,9 +19,8 @@ let myChart: Chart | null = null;
 const WeeklyChart = () => {
     const gameHistory = useSelector((state: any) => state.historyReducer.gameHistory);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    // Create a new array that only includes entries with a non-null, non-empty level
     const validEntries = gameHistory.filter((entry: GameEntry) => entry.level);
-    //console.log('All dates:', validEntries.map((entry: GameEntry) => entry.timestamp));
+    const [currentWeekStart, setCurrentWeekStart] = useState(startOfWeek(new Date(), { weekStartsOn: 1 }));
 
     const destroyChart = useCallback(() => {
         if (myChart) {
@@ -28,73 +29,30 @@ const WeeklyChart = () => {
         }
     }, []);
 
-    const calculateTotalScore = (level: string, day: number) => {
-        console.log('validEntries:: ', validEntries);
+    const calculateTotalScore = (level: string, day: number, weekStart: Date) => {
+        const weekEnd = endOfWeek(weekStart, { weekStartsOn: 1 });
         const filteredEntries = validEntries
             .filter((entry: GameEntry) => {
-
-                // Check if entry.timestamp is not undefined and not an empty string
-                if (!entry.timestamp || entry.timestamp.trim() === '') {
-                    console.log(`Invalid timestamp: ${entry.timestamp}`);
-                    return false;
-                }
-
-                console.log('Original timestamp:', entry.timestamp);
-
-                // Attempt to parse the timestamp as DD/MM/YYYY, HH:mm:ss
-                const [dateStr, timeStr] = entry.timestamp.split(', ');
-                const [dayStr, monthStr, yearStr] = dateStr.split('/');
-                const parsedDate = new Date(`${monthStr} ${dayStr} ${yearStr} ${timeStr}`);
-
-                // Check if the parsed date is valid
-                if (!isNaN(parsedDate.getTime())) {
-                    // Successfully parsed as DD/MM/YYYY, HH:mm:ss
-                    const entryDay = parsedDate.getDay(); // 0-6, where 0 is Sunday
-                    const isMatch = entry.level === level && entryDay === Number(day);
-                    console.log('Parsed date:', parsedDate, 'Comparison:', isMatch ? 'Match' : 'No Match');
-                    return isMatch;
-                } else {
-                    // Try parsing the timestamp as ISO 8601
-                    const isoDate = new Date(entry.timestamp);
-                    if (!isNaN(isoDate.getTime())) {
-                        // Successfully parsed as ISO 8601
-                        const entryDay = isoDate.getDay(); // 0-6, where 0 is Sunday
-                        const isMatch = entry.level === level && entryDay === Number(day);
-                        console.log('Parsed date (ISO 8601):', isoDate, 'Comparison:', isMatch ? 'Match' : 'No Match');
-                        return isMatch;
-                    } else {
-                        // Both parsing attempts failed
-                        console.log('Invalid timestamp format');
-                        return false;
-                    }
-                }
+                const timestamp = new Date(entry.timestamp);
+                const entryDay = timestamp.getDay();
+                return entry.level === level && entryDay === Number(day) && isWithinInterval(timestamp, { start: weekStart, end: weekEnd });
             });
 
-        const totalScore = filteredEntries
-            .reduce((total: number, entry: GameEntry) => total + entry.score, 0);
-        console.log(`Total score for level ${level} on day ${day}:`, totalScore);
-        return totalScore;
+        return filteredEntries.reduce((total: number, entry: GameEntry) => total + entry.score, 0);
     };
-
-
 
     useEffect(() => {
         if (canvasRef.current) {
             const ctx = canvasRef.current.getContext('2d');
-
-            // Check if there is a non-blank level in the gameHistory
             const nonBlankLevelData = gameHistory.filter((entry: GameEntry) => entry.level !== null);
 
             if (nonBlankLevelData.length === 0) {
-                // No non-blank level found, destroy the chart and return
                 destroyChart();
                 return;
             }
 
-            // Destroy the previous chart before creating a new one
             destroyChart();
 
-            // Create a new chart
             if (ctx) {
                 myChart = new Chart(ctx, {
                     type: 'bar',
@@ -107,21 +65,21 @@ const WeeklyChart = () => {
                         datasets: [
                             {
                                 label: 'Easy',
-                                data: [0, 1, 2, 3, 4, 5, 6].map(day => calculateTotalScore('Easy', day)),
+                                data: [0, 1, 2, 3, 4, 5, 6].map(day => calculateTotalScore('Easy', day, currentWeekStart)),
                                 backgroundColor: 'rgba(75,192,192,0.4)',
                                 borderColor: 'rgba(75,192,192,1)',
                                 borderWidth: 1,
                             },
                             {
                                 label: 'Medium',
-                                data: [0, 1, 2, 3, 4, 5, 6].map(day => calculateTotalScore('Medium', day)),
+                                data: [0, 1, 2, 3, 4, 5, 6].map(day => calculateTotalScore('Medium', day, currentWeekStart)),
                                 backgroundColor: 'rgba(192,75,75,0.4)',
                                 borderColor: 'rgba(192,75,75,1)',
                                 borderWidth: 1,
                             },
                             {
                                 label: 'Difficult',
-                                data: [0, 1, 2, 3, 4, 5, 6].map(day => calculateTotalScore('Difficult', day)),
+                                data: [0, 1, 2, 3, 4, 5, 6].map(day => calculateTotalScore('Difficult', day, currentWeekStart)),
                                 backgroundColor: 'rgba(75,75,192,0.4)',
                                 borderColor: 'rgba(75,75,192,1)',
                                 borderWidth: 1,
@@ -131,14 +89,22 @@ const WeeklyChart = () => {
                 });
             }
         }
-        // Cleanup function to destroy the chart when the component is unmounted or dependencies change
+
         return destroyChart;
-    }, [canvasRef, gameHistory, destroyChart]);
+    }, [canvasRef, gameHistory, destroyChart, currentWeekStart]);
 
     return (
         <div className={'chart-page'}>
             <div className={'chart-header'}>
-                <h2>Weekly Chart</h2>
+                <h1>Week: {getISOWeek(currentWeekStart)}</h1>
+                <div>
+                    <IconButton onClick={() => setCurrentWeekStart(weekStart => subWeeks(weekStart, 1))}>
+                        <ArrowLeft />
+                    </IconButton>
+                    <IconButton onClick={() => setCurrentWeekStart(weekStart => addWeeks(weekStart, 1))}>
+                        <ArrowRight />
+                    </IconButton>
+                </div>
                 <Link to="/protected"><Button variant={"outlined"} size={"large"}>Return Home</Button></Link>
             </div>
 
