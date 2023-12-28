672952b4a44e1bcaa099f9228c45aa1c3f648dbd
Nisha Chavan <nisha.chavan@lekab.com>
2023-12-28 13:43:54 +0100

    game welcome image changed

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,5 +1,7 @@
 import logo from './images/Nisha_Logo.png';
+import gcLogo from './images/GC-Logo.png';
 
 export default  {
-    logo
+    logo,
+    gcLogo
 }
--- src/containers/welcome/Welcome.tsx
+++ src/containers/welcome/Welcome.tsx
@@ -1,10 +1,10 @@
 import {useNavigate} from "react-router-dom";
 import React from "react";
-import logo from "../../Nisha_Logo.png";
 import Button from "@mui/material/Button";
 import { motion } from 'framer-motion';
 import AppWrap from "../../wrapper/AppWrap";
 import './welcome.scss'
+import {images} from '../../assets/index';
 function Welcome() {
     const navigate = useNavigate();
 
@@ -18,7 +18,7 @@ function Welcome() {
                     <motion.img
                         whileInView={{ scale: [0, 1] }}
                         transition={{ duration: 1, ease: 'easeInOut' }}
-                        src={logo} className="App-logo" alt="logo" />
+                        src={images.gcLogo} className="app__app-logo" alt="logo" />
                     <Button variant="outlined" size="large" onClick={handleClick}>Let's Start!</Button>
                 </header>
             </motion.div>
